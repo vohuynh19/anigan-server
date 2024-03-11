4436d295f9103e70ec5a162c0038f58b9dee14c3
VoHuynh <huynh.vo@unifiedpost.com>
2024-03-11 23:37:47 +0700

    fix out of memory

--- main.colab.py
+++ main.colab.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI
 from PIL import Image
 from pydantic import BaseModel
 
-# from torchvision import transforms
+from torchvision import transforms
 # from torchvision.utils import save_image
 
 from anigan.trainer import Trainer
@@ -92,31 +92,47 @@ class ProcessImageDataV2(BaseModel):
 def process_images(data: ProcessImageDataV2):
     # Add your image processing logic here
     
-    source_img = Image.open(BytesIO(requests.get(data.source_img_path).content)).convert('RGB')
-    input_tensor = torch.from_numpy(np.array(source_img)).unsqueeze(0).permute(0, 3, 1, 2).float() / 255.0
-    input_tensor = input_tensor.to("cuda")
+    input_image = Image.open(BytesIO(requests.get(data.source_img_path).content)).convert('RGB')
+    
+    target_width = 1024
+
+    # Calculate the target height based on the original aspect ratio
+    original_width, original_height = input_image.size
+    target_height = int((float(target_width) / original_width) * original_height)
+
+    transform_list = [
+        transforms.Resize((target_height, target_width)),  # Resize while maintaining aspect ratio
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ]
+
+    transform = transforms.Compose(transform_list)
+    input_tensor = transform(input_image).unsqueeze(0).cuda()
+    
+    
     output_dir = "result_dir"
     os.makedirs(output_dir, exist_ok=True)
     
     with torch.no_grad():
         output_tensor = modelV2(input_tensor)
-        save_file_path = os.path.join(output_dir, f"output.png")
-        output_image = output_tensor.detach().cpu().squeeze(0).permute(1, 2, 0).numpy()
-        output_image = (output_image * 255.0).clip(0, 255).astype(np.uint8)
-        output_image = Image.fromarray(output_image)
-        output_image.save(save_file_path)
-        print(f"Result is saved to: {save_file_path}")
-        # Upload to firebase
-        unique_id = str(uuid.uuid4())
-        path = f"processed/{unique_id}.png"
-        quoted_path = urllib.parse.quote(path, safe='')
-        blob = bucket.blob(path)
-        blob.upload_from_filename(save_file_path)
-        firebase_url =  f"https://firebasestorage.googleapis.com/v0/b/{bucket.name}/o/{quoted_path}?alt=media"
-        print(f"Image uploaded to Firebase: {firebase_url}")
-        return {
-            "processed_url" : firebase_url
-        }
+        
+    save_file_path = os.path.join(output_dir, f"output.png")
+    output_image = output_tensor.detach().cpu().squeeze(0).permute(1, 2, 0).numpy()
+    output_image = (output_image * 255.0).clip(0, 255).astype(np.uint8)
+    output_image = Image.fromarray(output_image)
+    output_image.save(save_file_path)
+    print(f"Result is saved to: {save_file_path}")
+    # Upload to firebase
+    unique_id = str(uuid.uuid4())
+    path = f"processed/{unique_id}.png"
+    quoted_path = urllib.parse.quote(path, safe='')
+    blob = bucket.blob(path)
+    blob.upload_from_filename(save_file_path)
+    firebase_url =  f"https://firebasestorage.googleapis.com/v0/b/{bucket.name}/o/{quoted_path}?alt=media"
+    print(f"Image uploaded to Firebase: {firebase_url}")
+    return {
+        "processed_url" : firebase_url
+    }
         
 cc = ColabCode(port=12000, code=False)
 cc.run_app(app=app)
